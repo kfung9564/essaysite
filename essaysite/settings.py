86441ec599472212728099d83ae51de6896d836b
Kwan <kfung9564@bths.edu>
2020-12-21 22:26:02 -0500

    django-heroku

--- essaysite/settings.py
+++ essaysite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,3 +118,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'userpage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
